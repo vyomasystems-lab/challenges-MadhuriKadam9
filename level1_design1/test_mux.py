369f55b0f2a903f2ca1bf9d487bd41e6267aa82f
Madhuri Kadam <madhurib.saksham@gmail.com>
2022-07-28 15:16:09 +0000

    two test

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -37,4 +37,23 @@ async def test_mux(dut):
 
      dut._log.info(f'INP0={INP0:05} INP1={INP1:05} INP2={INP2:05} INP3={INP3:05}  SEL={SEL:05} model={int(dut.inp3.value):05} DUT={int(dut.out.value):05}')
     assert dut.out.value == dut.inp3.value, "Randomised test failed with: {INP3} = {OUT}".format(INP0=dut.inp0.value,INP1=dut.inp1.value,INP2=dut.inp2.value, INP3=dut.inp3.value, OUT=dut.out.value)
+
+@cocotb.test()
+async def adder_basic_test(dut):
+    """Test for 5 + 10"""
+
+    INP2 = 3
+    SEL = 2
+
+    # input driving
+    dut.inp2.value = INP2
+    dut.sel.value = SEL
+
+    await Timer(2, units='ns')
     
+    assert dut.out.value == INP2, "Mux result is incorrect: {INP2} != {SUM}, expected value={EXP}".format(
+            INP2=int(dut.inp2.value), SEL=int(dut.sel.value), OUT=int(dut.out.value), EXP=INP2)
+
+
+
+
